1261803cdc8a8eaf28a8a2262912b45cc7621a67
Hareendran MG <hareendranmg@gmail.com>
2026-01-12 11:24:51 +0530

    feat: Add system tray option

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,11 @@
-use tauri::{Manager, Window};
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    Manager, Window, Emitter,
+};
 use std::{thread, time::Duration};
 use tauri_plugin_autostart::MacosLauncher;
 use tauri_plugin_autostart::ManagerExt;
-use tauri::Emitter;
 
 #[tauri::command]
 async fn hide_window(window: Window) {
@@ -25,14 +28,51 @@ pub fn run() {
         .setup(|app| {
             // Enable autostart on startup
             let _ = app.autolaunch().enable();
+            
+            // System Tray Setup
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>).unwrap();
+            let show_i = MenuItem::with_id(app, "show", "Open Water Reminder", true, None::<&str>).unwrap();
+            let menu = Menu::with_items(app, &[&show_i, &quit_i]).unwrap();
+
+            let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .show_menu_on_left_click(false)
+                .icon(app.default_window_icon().unwrap().clone())
+                .on_menu_event(|app, event| {
+                    match event.id.as_ref() {
+                        "quit" => app.exit(0),
+                        "show" => {
+                            if let Some(window) = app.get_webview_window("main") {
+                                window.show().unwrap();
+                                window.set_focus().unwrap();
+                            }
+                        }
+                        _ => {}
+                    }
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                })
+                .build(app);
+
 
             let app_handle = app.handle().clone();
             // Spawn a thread to show the window every hour
             std::thread::spawn(move || {
                 loop {
                     // Wait for 1 hour
-                    thread::sleep(Duration::from_secs(3600));
-
+                    thread::sleep(Duration::from_secs(3600)); 
+                    
                     if let Some(window) = app_handle.get_webview_window("main") {
                         let _ = window.show();
                         let _ = window.set_focus();
